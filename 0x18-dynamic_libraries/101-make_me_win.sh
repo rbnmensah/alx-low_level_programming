3326411cb3994e50628ef6a9b53e7b0a5789a1df
Reuben <rbnmensah@gmail.com>
2023-08-26 02:11:45 +0000

    extras

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,2 @@
-wget -P /tmp https://github.com/rbnmensah/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+wget -P /alx-low_level_programming https://github.com/rbnmensah/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/alx-low_level_programming/libgiga.so
